32cb5a22413f643f5066ae2bee35223d162db67d
panchengrui <1609003519@qq.com>
2022-01-09 16:20:50 +0800

    【add】first-commit

---
+++ src/test/java/study/pcr/elk/ElkApplicationTests.java
@@ -0,0 +1,13 @@
+package study.pcr.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
